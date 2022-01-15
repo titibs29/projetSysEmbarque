6bf72099696d988643befa71ae7d04240aab67c3
titibs29 <thibaut.rizzoli@hotmail.be>
2022-01-15 15:49:34 +0100

    nettoyage des libs rtc

--- libs/rtc.cpp
+++ libs/rtc.cpp
@@ -1,82 +1,74 @@
-﻿										///////////CODE RTC/////////
+﻿///////////CODE RTC/////////
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "rtc.h"
 
-
-#define slave_Add 0b1101000 //0x68
+#define slave_Add 0b1101000 // 0x68
 using namespace std;
-
-
 
 int slave_Address;
 
-					//Initialisation de la librairie pour avoir l'heure et les dates actuelles//
+// Initialisation de la librairie pour avoir l'heure et les dates actuelles//
 time_t t = time(NULL);
-tm* tPtr = localtime(&t);
-					//Initialisation de la librairie pour avoir l'heure et les dates actuelles//
-/* int main(void)
+tm *tPtr = localtime(&t);
+// Initialisation de la librairie pour avoir l'heure et les dates actuelles//
 
+string give_day_of_week(int day)
 {
-	
-
-	wiringPiSetupGpio();
-	slave_Address = wiringPiI2CSetup(slave_Add);
-
-	int heure = 0, minute = 0, seconde = 0, day = 0, date = 0, month = 0, year = 0;
-
-	RTC_Init(seconde, minute, heure, day, date, month, year); //Initialisation aux valeurs envoyées (valeurs actuelles)
-
-	while (true)
+	switch (day)
 	{
-		
-		int sec = RTC_read(slave_Address, 0); //sec
-		int min = RTC_read(slave_Address, 1); //min
-		int h = RTC_read(slave_Address, 2); //heures 
-		int day = RTC_read(slave_Address, 3); //Day
-		int date = RTC_read(slave_Address, 4); //Date
-		int month = RTC_read(slave_Address, 5); //Month
-		int year = RTC_read(slave_Address, 6); // Year
-
-		cout << give_day_of_week(transfoDCB_To_Dec(day)) << " " << transfoDCB_To_Dec(date) << " " << give_month_of_year(transfoDCB_To_Dec(month)) << " " <<20<<transfoDCB_To_Dec(year) << "~~" << transfoDCB_To_Dec(h) << ":" << transfoDCB_To_Dec(min) << ":" << transfoDCB_To_Dec(sec) << ":" << endl;
-		delay(1000);
-	}
-	return 0;
-}
- */
-
-string give_day_of_week(int day) {
-	switch (day) {
-	case 1: return "LUN";
-	case 2: return "MAR";
-	case 3: return "MER";
-	case 4: return "JEU";
-	case 5: return "VEN";
-	case 6: return "SAM";
-	case 7: return "DIM";
-	default: return "   ";
+	case 1:
+		return "LUN";
+	case 2:
+		return "MAR";
+	case 3:
+		return "MER";
+	case 4:
+		return "JEU";
+	case 5:
+		return "VEN";
+	case 6:
+		return "SAM";
+	case 7:
+		return "DIM";
+	default:
+		return "   ";
 	}
 }
-string give_month_of_year(int month) {
+string give_month_of_year(int month)
+{
 	switch (month)
 	{
-	case 1: return "Jan";
-	case 2: return "Feb";
-	case 3: return "Mar";
-	case 4: return "Apr";
-	case 5: return "May";
-	case 6: return "June";
-	case 7: return "Jul";
-	case 8: return "Aug";
-	case 9: return "Sept";
-	case 10: return "Oct";
-	case 11: return "Nov";
-	case 12: return "Dec";
-	default: return "   ";
+	case 1:
+		return "Jan";
+	case 2:
+		return "Feb";
+	case 3:
+		return "Mar";
+	case 4:
+		return "Apr";
+	case 5:
+		return "May";
+	case 6:
+		return "June";
+	case 7:
+		return "Jul";
+	case 8:
+		return "Aug";
+	case 9:
+		return "Sept";
+	case 10:
+		return "Oct";
+	case 11:
+		return "Nov";
+	case 12:
+		return "Dec";
+	default:
+		return "   ";
 	}
 }
-//Initialisation et fonction de vérification
+// Initialisation et fonction de vérification
 void RTC_Init(int secondes, int minutes, int heures, int day, int date, int month, int year)
 {
 	if (heures >= 23)
@@ -103,13 +95,13 @@ void RTC_Init(int secondes, int minutes, int heures, int day, int date, int mont
 	{
 		month = month % 12;
 	}
-	RTC_Send(slave_Address, 0, transfo_Dec_To_DCB(tPtr->tm_sec)); //Seconde
-	RTC_Send(slave_Address, 1, transfo_Dec_To_DCB(tPtr->tm_min)); //Minute
-	RTC_Send(slave_Address, 2, transfo_Dec_To_DCB(tPtr->tm_hour)); //hour
-	RTC_Send(slave_Address, 3, transfo_Dec_To_DCB(tPtr->tm_wday)); //Day of week
-	RTC_Send(slave_Address, 4, transfo_Dec_To_DCB(tPtr->tm_mday)); //Date of month 
-	RTC_Send(slave_Address, 5, transfo_Dec_To_DCB(tPtr->tm_mon) + 1); //Month
-	RTC_Send(slave_Address, 6, transfo_Dec_To_DCB(21)); //year
+	RTC_Send(slave_Address, 0, transfo_Dec_To_DCB(tPtr->tm_sec));	  // Seconde
+	RTC_Send(slave_Address, 1, transfo_Dec_To_DCB(tPtr->tm_min));	  // Minute
+	RTC_Send(slave_Address, 2, transfo_Dec_To_DCB(tPtr->tm_hour));	  // hour
+	RTC_Send(slave_Address, 3, transfo_Dec_To_DCB(tPtr->tm_wday));	  // Day of week
+	RTC_Send(slave_Address, 4, transfo_Dec_To_DCB(tPtr->tm_mday));	  // Date of month
+	RTC_Send(slave_Address, 5, transfo_Dec_To_DCB(tPtr->tm_mon) + 1); // Month
+	RTC_Send(slave_Address, 6, transfo_Dec_To_DCB(21));				  // year
 }
 
 int RTC_read(int address, int reg)
@@ -134,12 +126,10 @@ int transfoDCB_To_Dec(int data)
 int transfo_Dec_To_DCB(int number)
 {
 	int unit, ten, DCB = 0;
-	unit = number % 10; //Récupération de l'unité
-	ten = (number - unit) / 10; //Récupération de la dizaine
-	DCB = DCB + ten; //On ajoute la dizaine ...
-	DCB = DCB << 4; //..On décale pour la mettre à droite
-	DCB = DCB + unit; // On ajoute l'unité
+	unit = number % 10;			// Récupération de l'unité
+	ten = (number - unit) / 10; // Récupération de la dizaine
+	DCB = DCB + ten;			// On ajoute la dizaine ...
+	DCB = DCB << 4;				//..On décale pour la mettre à droite
+	DCB = DCB + unit;			// On ajoute l'unité
 	return DCB;
 }
-
-
--- libs/rtc.h
+++ libs/rtc.h
@@ -1,16 +1,42 @@
-#include<cmath>
-#include <wiringPi.h>
 #include <wiringPiI2C.h>
-#include <iostream>
-#include <ctime>
 #include <chrono>
 #include <string>
 
-void RTC_Send(int address, int regis, int data);  //Envoi de valeur dans la RTC
-int RTC_read(int address, int reg); //Lecture de ces valeurs
-void RTC_Init(int secondes, int minutes, int heures, int day, int date, int month, int year); //Utilisation de RTC_Send pour initialiser à 0 
+void RTC_Send(int address, int regis, int data);                                              // Envoi de valeur dans la RTC
+int RTC_read(int address, int reg);                                                           // Lecture de ces valeurs
+void RTC_Init(int secondes, int minutes, int heures, int day, int date, int month, int year); // Utilisation de RTC_Send pour initialiser à 0
 std::string give_day_of_week(int day);
 std::string give_month_of_year(int month);
 
-int transfo_Dec_To_DCB(int number); //Ecriture dans la RTC --> Envoi en format DCB
-int transfoDCB_To_Dec(int number);  //Lecture --> Conversion de DCB en decimal
+int transfo_Dec_To_DCB(int number); // Ecriture dans la RTC --> Envoi en format DCB
+int transfoDCB_To_Dec(int number);  // Lecture --> Conversion de DCB en decimal
+
+/* int main(void)
+
+{
+
+
+    wiringPiSetupGpio();
+    slave_Address = wiringPiI2CSetup(slave_Add);
+
+    int heure = 0, minute = 0, seconde = 0, day = 0, date = 0, month = 0, year = 0;
+
+    RTC_Init(seconde, minute, heure, day, date, month, year); //Initialisation aux valeurs envoyées (valeurs actuelles)
+
+    while (true)
+    {
+
+        int sec = RTC_read(slave_Address, 0); //sec
+        int min = RTC_read(slave_Address, 1); //min
+        int h = RTC_read(slave_Address, 2); //heures
+        int day = RTC_read(slave_Address, 3); //Day
+        int date = RTC_read(slave_Address, 4); //Date
+        int month = RTC_read(slave_Address, 5); //Month
+        int year = RTC_read(slave_Address, 6); // Year
+
+        cout << give_day_of_week(transfoDCB_To_Dec(day)) << " " << transfoDCB_To_Dec(date) << " " << give_month_of_year(transfoDCB_To_Dec(month)) << " " <<20<<transfoDCB_To_Dec(year) << "~~" << transfoDCB_To_Dec(h) << ":" << transfoDCB_To_Dec(min) << ":" << transfoDCB_To_Dec(sec) << ":" << endl;
+        delay(1000);
+    }
+    return 0;
+}
+ */
